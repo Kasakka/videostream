209a1071a6b4efd6677e0efa7912e98fcb82ca1e
Kasakka <henri.matero@gmail.com>
2025-02-20 20:15:12 +0200

    Initial commit

---
+++ VideoStreaming/Controllers/VideoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoStreaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private const string VideoFilePath = "videos/unreal.mp4";
+
+        [HttpGet("stream")]
+        public IActionResult StreamVideo()
+        {
+            if (!System.IO.File.Exists(VideoFilePath))
+            {
+                return NotFound();
+            }
+
+            var fileStream = new FileStream(VideoFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            return File(fileStream, "video/mp4", enableRangeProcessing: true);
+        }
+    }
+}
